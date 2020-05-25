fb03bb32d82a9044d4b70ab92fabc488c4186790
rahulabrol2211 <rahulabrol2211@gmail.com>
2020-05-25 21:10:12 +0530

    gifs added for dialog

--- app/src/main/java/com/rahulabrol/materialdailog/MainActivity.kt
+++ app/src/main/java/com/rahulabrol/materialdailog/MainActivity.kt
@@ -10,73 +10,80 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var simpleDialog: MaterialDialog? = null
+    private var animatedDialog: MaterialDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Simple Material Dialog
+        simpleDialog = MaterialDialog.Builder(this)
+            .setTitle("Delete?")
+            .setMessage("Are you sure want to delete this file?")
+            .setCancelable(false)
+            .setPositiveButton(
+                "Delete",
+                R.drawable.ic_delete,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+
+                    }
+                })
+            .setNegativeButton(
+                "Cancel",
+                R.drawable.ic_close,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+                    }
+                })
+            .build()
+
+        // Animated Simple Material Dialog
+        animatedDialog = MaterialDialog.Builder(this)
+            .setTitle("Delete?")
+            .setMessage("Are you sure want to delete this file?")
+            .setCancelable(false)
+            .setPositiveButton(
+                "Delete",
+                R.drawable.ic_delete,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+                    }
+                })
+            .setNegativeButton(
+                "Cancel",
+                R.drawable.ic_close,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+                    }
+                })
+            .setAnimation("delete_anim.json")
+            .build()
+
         button_simple_dialog.setOnClickListener {
-            // Simple Material Dialog
-
-            // Simple Material Dialog
-            MaterialDialog.Builder(this)
-                .setTitle("Delete?")
-                .setMessage("Are you sure want to delete this file?")
-                .setCancelable(false)
-                .setPositiveButton(
-                    "Delete",
-                    R.drawable.ic_delete,
-                    object : AbstractDialog.OnClickListener {
-                        override fun onClick(dialogInterface: DialogInterface?, which: Int) {
-                            Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
-                                .show()
-                            dialogInterface?.dismiss()
-
-                        }
-                    })
-                .setNegativeButton(
-                    "Cancel",
-                    R.drawable.ic_close,
-                    object : AbstractDialog.OnClickListener {
-                        override fun onClick(dialogInterface: DialogInterface?, which: Int) {
-                            Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
-                                .show()
-                            dialogInterface?.dismiss()
-                        }
-                    })
-                .build()
-                .show()
+            // Show simple Material Dialog
+            simpleDialog?.show()
         }
 
         button_animated_dialog.setOnClickListener {
             // Animated Simple Material Dialog
-
-            // Animated Simple Material Dialog
-            MaterialDialog.Builder(this)
-                .setTitle("Delete?")
-                .setMessage("Are you sure want to delete this file?")
-                .setCancelable(false)
-                .setPositiveButton(
-                    "Delete",
-                    R.drawable.ic_delete,
-                    object : AbstractDialog.OnClickListener {
-                        override fun onClick(dialogInterface: DialogInterface?, which: Int) {
-                            Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
-                                .show()
-                            dialogInterface?.dismiss()
-                        }
-                    })
-                .setNegativeButton(
-                    "Cancel",
-                    R.drawable.ic_close,
-                    object : AbstractDialog.OnClickListener {
-                        override fun onClick(dialogInterface: DialogInterface?, which: Int) {
-                            Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
-                                .show()
-                            dialogInterface?.dismiss()
-                        }
-                    })
-                .setAnimation("delete_anim.json")
-                .build()
-                .show()
+            animatedDialog?.show()
         }
+    }
+
+    companion object {
+        const val IS_SHOWING_DIALOG = "IS_SHOWING_DIALOG"
     }
 }
