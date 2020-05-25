4e0bb87dca4d6a6791fa70b33b6c693d86bdbdd6
rahulabrol2211 <rahulabrol2211@gmail.com>
2020-05-25 22:46:18 +0530

    bottom sheet code updated

--- app/src/main/java/com/rahulabrol/materialdailog/MainActivity.kt
+++ app/src/main/java/com/rahulabrol/materialdailog/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.rahulabrol.materialdialog.AbstractDialog
+import com.rahulabrol.materialdialog.MaterialBottomSheet
 import com.rahulabrol.materialdialog.MaterialDialog
 import com.rahulabrol.materialdialog.interfaces.DialogInterface
 import kotlinx.android.synthetic.main.activity_main.*
@@ -79,18 +80,27 @@ class MainActivity : AppCompatActivity() {
             .setTitle("Delete?")
             .setMessage("Are you sure want to delete this file?")
             .setCancelable(false)
-            .setPositiveButton("Delete", R.drawable.ic_delete, object : OnClickListener() {
-                fun onClick(dialogInterface: DialogInterface, i: Int) {
-                    Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT).show()
-                    dialogInterface.dismiss()
-                }
-            })
-            .setNegativeButton("Cancel", R.drawable.ic_close, object : OnClickListener() {
-                fun onClick(dialogInterface: DialogInterface, which: Int) {
-                    Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT).show()
-                    dialogInterface.dismiss()
-                }
-            })
+            .setPositiveButton(
+                "Delete",
+                R.drawable.ic_delete,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+
+                    }
+                })
+            .setNegativeButton(
+                "Cancel",
+                R.drawable.ic_close,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+                    }
+                })
             .build()
 
 
@@ -99,18 +109,27 @@ class MainActivity : AppCompatActivity() {
             .setTitle("Delete?")
             .setMessage("Are you sure want to delete this file?")
             .setCancelable(false)
-            .setPositiveButton("Delete", R.drawable.ic_delete, object : OnClickListener() {
-                fun onClick(dialogInterface: DialogInterface, i: Int) {
-                    Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT).show()
-                    dialogInterface.dismiss()
-                }
-            })
-            .setNegativeButton("Cancel", R.drawable.ic_close, object : OnClickListener() {
-                fun onClick(dialogInterface: DialogInterface, which: Int) {
-                    Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT).show()
-                    dialogInterface.dismiss()
-                }
-            })
+            .setPositiveButton(
+                "Delete",
+                R.drawable.ic_delete,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Deleted!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+
+                    }
+                })
+            .setNegativeButton(
+                "Cancel",
+                R.drawable.ic_close,
+                object : AbstractDialog.OnClickListener {
+                    override fun onClick(dialogInterface: DialogInterface?, which: Int) {
+                        Toast.makeText(applicationContext, "Cancelled!", Toast.LENGTH_SHORT)
+                            .show()
+                        dialogInterface?.dismiss()
+                    }
+                })
             .setAnimation("delete_anim.json")
             .build()
 
@@ -119,13 +138,19 @@ class MainActivity : AppCompatActivity() {
             simpleDialog?.show()
         }
 
-        button_animated_dialog.setOnClickListener {
-            // Animated Simple Material Dialog
-            animatedDialog?.show()
+        button_simple_dialog.setOnClickListener {
+            // Show simple Material Dialog
+            simpleDialog?.show()
         }
-    }
 
-    companion object {
-        const val IS_SHOWING_DIALOG = "IS_SHOWING_DIALOG"
+        button_simple_bottomsheet_dialog.setOnClickListener {
+            // Show simple Material Dialog
+            mSimpleBottomSheetDialog?.show()
+        }
+
+        button_animated_bottomsheet_dialog.setOnClickListener {
+            // Animated Simple Material Dialog
+            mAnimatedBottomSheetDialog?.show()
+        }
     }
 }
